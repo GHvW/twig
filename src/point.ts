ce4eed1f76d5f2b578a6ba86c108e75783496711
Garrett van Wageningen <ghvanw283@gmail.com>
2021-04-07 23:03:58 -0500

    wip update to all things

--- src/point.ts
+++ src/point.ts
@@ -3,69 +3,66 @@ type II = 2;
 type III = 3;
 type IV = 4;
 
-type Quadrant
+export type Quadrant
   = I 
   | II
   | III
   | IV
 
-interface IPoint {
+export interface IPoint {
   x: number;
   y: number;
 }
 
-class Point implements IPoint {
-  x: number;
-  y: number;
+export interface ILine {
+    p1: IPoint;
+    p2: IPoint;
+}
 
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
-  }
 
-  distanceTo(other: Point): number {
+export type Vector = [x: number, y: number];
+
+
+export function distance({ p1, p2 }: ILine): number {
     return Math.sqrt(
-      Math.pow(other.x - this.x, 2) + Math.pow(other.y - this.y, 2)
+      Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2)
     );
-  }
+}
 
-  quadrantOf(other: Point): Quadrant {
-    if (other.x >= this.x && other.y >= this.y) {
+export function directionRelativeToOrigin({ p1, p2 }: ILine): Quadrant {
+    if (p2.x >= p1.x && p2.y >= p1.y) {
       return 1;
-    } else if (other.x <= this.x && other.y > this.y) {
+    } else if (p2.x <= p1.x && p2.y > p1.y) {
       return 2;
-    } else if (other.x < this.x && other.y <= this.y) {
+    } else if (p2.x < p1.x && p2.y <= p1.y) {
       return 3;
-    } else {
-      return 4
     }
-  }
+    return 4;
+}
 
-  getPointFrom(quadrant: Quadrant, opposite: number, adjacent: number): Point {
-    switch (quadrant) {
+
+export function translatePoint(direction: Quadrant, vector: Vector, { x, y }: IPoint): IPoint {
+    switch (direction) {
       case 1:
-        return new Point(
-          this.x + adjacent,
-          this.y + opposite
-        );
+        return {
+          x: x + vector[0],
+          y: y + vector[1]
+        };
       case 2:
-        return new Point(
-          this.x - adjacent,
-          this.y + opposite
-        );
+        return {
+          x: x - vector[0],
+          y: y + vector[1]
+        };
       case 3:
-        return new Point(
-          this.x - adjacent,
-          this.y - opposite
-        );
+        return {
+          x: x - vector[0],
+          y: y - vector[1]
+        };
       case 4:
-        return new Point(
-          this.x + adjacent,
-          this.y - opposite
-        );
-    }
+        return {
+          x: x + vector[0],
+          y: y - vector[1]
+        };
     // typescript's --strictNullChecks should check for exhaustiveness
   }
 }
-
-export default Point;
--- src/polyline.ts
+++ src/polyline.ts
@@ -1,43 +1,80 @@
-import Point from "./point";
+import { IPoint, distance } from "./point";
 
-class Polyline {
-  path: Array<Point>
 
-  constructor(path: Array<Point>) {
-    this.path = path;
-  }
+type Polyline = Array<IPoint>;
 
-  totalDistance(): number {
-    return this.path.reduce((total, point, index, thisArray) => {
-      if (index + 1 >= thisArray.length) {
-        return total;
-      }
-      return total + point.distanceTo(thisArray[index + 1]);
-    }, 0);
-  }
 
-  midPoint(): Point {
-    return this.reduceToMid(this.path, this.totalDistance() / 2);
-  }
-
-  private reduceToMid(path: Array<Point>, distanceLeft: number): Point {
-    const point1 = path[0];
-    const point2 = path[1];
-    const segmentLength = point1.distanceTo(point2);
-
-    if (segmentLength >= distanceLeft) {
-      const opposite = point2.y - point1.y;
-      const adjacent = point2.x = point1.x;
-      const radians = Math.abs(Math.atan(opposite / adjacent));
-
-      const oppositeOfDistanceRemaining = Math.sin(radians) * distanceLeft;
-      const adjacentOfDistanceRemaining = Math.cos(radians) * distanceLeft;
-
-      return point1.getPointFrom(point1.quadrantOf(point2), oppositeOfDistanceRemaining, adjacentOfDistanceRemaining);
+function totalDistance(line: Polyline): number {
+  return line.reduce((total, point, index) => {
+    if (index + 1 >= line.length) {
+      return total;
     }
-
-    return this.reduceToMid(path.slice(1), distanceLeft - segmentLength);
-  }
+    return total + distance({ p1: point, p2: line[index + 1]});
+  }, 0); 
 }
 
-export default Polyline;
+function midpoint(line: Polyline): IPoint {
+  return line.reduceToMid(this.path, this.totalDistance() / 2);
+}
+
+
+function reduceToMid(path: Polyline, distanceLeft: number): IPoint {
+  const point1 = path[0];
+  const point2 = path[1];
+  const segmentLength = distance({ p1: point1, p2: point2});
+
+  if (segmentLength >= distanceLeft) {
+    const opposite = point2.y - point1.y;
+    const adjacent = point2.x = point1.x;
+    const radians = Math.abs(Math.atan(opposite / adjacent));
+
+    const oppositeOfDistanceRemaining = Math.sin(radians) * distanceLeft;
+    const adjacentOfDistanceRemaining = Math.cos(radians) * distanceLeft;
+
+    return point1.getPointFrom(point1.quadrantOf(point2), oppositeOfDistanceRemaining, adjacentOfDistanceRemaining);
+  }
+
+  return reduceToMid(path.slice(1), distanceLeft - segmentLength);
+}
+}
+// class Polyline {
+//   path: Array<Point>
+
+  // constructor(path: Array<Point>) {
+  //   this.path = path;
+  // }
+
+//   totalDistance(): number {
+//     return this.path.reduce((total, point, index, thisArray) => {
+//       if (index + 1 >= thisArray.length) {
+//         return total;
+//       }
+//       return total + point.distanceTo(thisArray[index + 1]);
+//     }, 0);
+//   }
+
+//   midPoint(): Point {
+//     return this.reduceToMid(this.path, this.totalDistance() / 2);
+//   }
+
+//   private reduceToMid(path: Array<Point>, distanceLeft: number): Point {
+//     const point1 = path[0];
+//     const point2 = path[1];
+//     const segmentLength = point1.distanceTo(point2);
+
+//     if (segmentLength >= distanceLeft) {
+//       const opposite = point2.y - point1.y;
+//       const adjacent = point2.x = point1.x;
+//       const radians = Math.abs(Math.atan(opposite / adjacent));
+
+//       const oppositeOfDistanceRemaining = Math.sin(radians) * distanceLeft;
+//       const adjacentOfDistanceRemaining = Math.cos(radians) * distanceLeft;
+
+//       return point1.getPointFrom(point1.quadrantOf(point2), oppositeOfDistanceRemaining, adjacentOfDistanceRemaining);
+//     }
+
+//     return this.reduceToMid(path.slice(1), distanceLeft - segmentLength);
+//   }
+// }
+
+// export default Polyline;
