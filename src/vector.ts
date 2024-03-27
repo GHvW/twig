b7c12a2e1bd18a575edce78a7e76afd38235573f
Garrett van Wageningen <ghvanw283@gmail.com>
2024-03-27 11:38:14 -0500

    add vec ops WIP

---
+++ src/vector.ts
@@ -0,0 +1,11 @@
+import { IPoint } from "./point";
+
+export interface IVector extends IPoint {}
+
+export function add(v1: IVector, v2: IVector): IVector {
+    return { x: v1.x + v2.x, y: v1.y + v2.y };
+}
+
+export function subtract(v1: IVector, v2: IVector): IVector {
+    return { x: v1.x - v2.x, y: v1.y - v2.y };
+}
