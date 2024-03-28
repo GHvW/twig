b64f5396c7b9effa266a5937e570b74742926112
Garrett van Wageningen <ghvanw283@gmail.com>
2024-03-27 19:20:43 -0500

    updating tests to use vitest

--- src/vector.ts
+++ src/vector.ts
@@ -3,9 +3,15 @@ import { IPoint } from "./point";
 export interface IVector extends IPoint {}
 
 export function add(v1: IVector, v2: IVector): IVector {
-    return { x: v1.x + v2.x, y: v1.y + v2.y };
+    return { 
+        x: v1.x + v2.x, 
+        y: v1.y + v2.y 
+    };
 }
 
 export function subtract(v1: IVector, v2: IVector): IVector {
-    return { x: v1.x - v2.x, y: v1.y - v2.y };
+    return { 
+        x: v1.x - v2.x, 
+        y: v1.y - v2.y 
+    };
 }
--- test/point.test.ts
+++ test/point.test.ts
@@ -1,5 +1,5 @@
-const { distance, generateDirection, translatePoint } = require("../dist/point");
-
+import { test, expect, describe } from "vitest";
+import { generateDirection, distance, translatePoint } from "../src/point";
 
 test("given two points, distance returns the distance between the two points", () => {
     expect(distance({ x: 0, y: 10 }, {x: 0, y: 0 })).toBe(10);
